00d1726247cd9d1feccce66a0dc1c873c3560a39
Abdurrasyid Muhasibi <com.syid@gmail.com>
2023-03-06 11:10:10 +0700

    control flow

---
+++ 5. Control Flow/control_flow.dart
@@ -0,0 +1,141 @@
+void main() {
+  // var number = 1;
+  // var numbers = [1, 2, 3, 4, 5];
+
+  // if (condition) { statement }
+  // if (number > 0) {
+  //   print('Number is positive');
+  // }
+
+  // if (condition) { statement } else { statement }
+  // if (number < 0) {
+  //   print('Number is negative');
+  // } else {
+  //   print('Number is positive');
+  // }
+
+  // if (condition) { statement } else if (condition) { statement } else { statement }
+  // if (number < 0) {
+  //   print('Number is negative');
+  // } else if (number > 0) {
+  //   print('Number is positive');
+  // } else {
+  //   print('Number is zero');
+  // }
+
+  // Ternary Operator
+  // var result = 0;
+  // var a = 1;
+  // var b = 2;
+
+  // if-else
+  // if (a > b) {
+  //   result = a;
+  // } else {
+  //   result = b;
+  // }
+  // print(result);
+
+  // condition ? expression1 : expression2
+  // result = a > b ? a : b;
+  // print(result);
+
+  // For Loop
+  // for (iterator; condition; increment/decrement) { statement }
+  // for (var i = 1; i <= 5; i++) {
+  //   print(i);
+  // }
+
+  // For Loop with index
+  // for (var i = 0; i < numbers.length; i++) {
+  //   print(numbers[i]);
+  // }
+
+  // For Loop without index
+  // for (var item in list) { statement }
+  // for (var number in numbers) {
+  //   print(number);
+  // }
+
+  // Conditional For Loop
+  // for (var number in numbers) {
+  //   if (number % 2 == 0) {
+  //     print(number);
+  //   }
+  // }
+
+  // While Loop
+  // while (condition) { statement }
+  // var i = 1;
+  // while (i <= 5) {
+  //   print(i);
+  //   i++;
+  // }
+
+  // While Loop with index
+  // var i = 0;
+  // while (i < numbers.length) {
+  //   print(numbers[i]);
+  //   i++;
+  // }
+
+  // Do-While Loop
+  // do { statement } while (condition);
+  // var i = 1;
+  // do {
+  //   print(i);
+  //   i++;
+  // } while (i <= 5);
+
+  // Do-While Loop with index
+  // var i = 0;
+  // do {
+  //   print(numbers[i]);
+  //   i++;
+  // } while (i < numbers.length);
+
+  // Break
+  // for (var number in numbers) {
+  //   if (number % 2 == 0) {
+  //     break;
+  //   }
+  //   print(number);
+  // }
+
+  // Continue
+  // for (var number in numbers) {
+  //   if (number % 2 == 0) {
+  //     continue;
+  //   }
+  //   print(number);
+  // }
+
+  // Switch
+  // switch (expression) {
+  //   case value1:
+  //     statement;
+  //     break;
+  //   case value2:
+  //     statement;
+  //     break;
+  //   default:
+  //     statement;
+  // }
+  // switch (number) {
+  //   case 1:
+  //     print('One');
+  //     break;
+  //   case 2:
+  //     print('Two');
+  //     break;
+  //   default:
+  //     print('Invalid');
+  // }
+
+  // Assertion
+  // var variable;
+  // print(variable);
+  // assert(variable != null, 'Variable is null');
+  // variable = 1;
+  // print(variable);
+}
