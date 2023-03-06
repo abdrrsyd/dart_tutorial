8f22f13ba8d19ca9d112ec85264d681166212603
Abdurrasyid Muhasibi <com.syid@gmail.com>
2023-03-06 08:55:00 +0700

    operators

---
+++ 3. Operators/operators.dart
@@ -0,0 +1,81 @@
+void main() {
+  // var operand1 = 1;
+  // var operand2 = 2;
+
+  // Arithmetic Operators
+  // print(-operand1);
+  // print(-operand2);
+  // print(operand1 + operand2);
+  // print(operand1 - operand2);
+  // print(operand1 * operand2);
+  // print(operand1 / operand2);
+  // print(operand1 ~/ operand2);
+  // print(operand1 % operand2);
+
+  // Prefix & Postfix Operators
+  // var prefixIncrement = 2;
+  // var postfixIncrement = 2;
+
+  // print(++prefixIncrement); // 2 + 1 = 3
+  // print(postfixIncrement++); // 2
+
+  // var prefixDecrement = 2;
+  // var postfixDecrement = 2;
+
+  // print(--prefixDecrement); // 2 - 1 = 1
+  // print(postfixDecrement--); // 2
+
+  // Relational Operators
+  // print(operand1 > operand2);
+  // print(operand1 < operand2);
+  // print(operand1 >= operand2);
+  // print(operand1 <= operand2);
+
+  // Equality Operators
+  // print(operand1 == operand2);
+  // print(operand1 != operand2);
+
+  // Type Test Operators
+  // int type1 = 1;
+  // double type2 = 1.0;
+  // String type3 = "1";
+  // bool type4 = true;
+
+  // print(type1 is int);
+  // print(type1 is! double);
+  // print(type2 is double);
+  // print(type2 is! int);
+  // print(type3 is String);
+  // print(type4 is bool);
+
+  // Assignment Operators
+  // print(operand1 += operand2);
+  // print(operand1 -= operand2);
+  // print(operand1 *= operand2);
+  // print(operand1 /= operand2);
+  // print(operand1 ~/= operand2);
+  // print(operand1 %= operand2);
+  // print(operand1 <<= operand2);
+  // print(operand1 >>= operand2);
+  // print(operand1 |= operand2);
+  // print(operand1 &= operand2);
+  // print(operand1 ^= operand2);
+
+  // Logical Operators
+  // print(!false);
+  // print(!true);
+  // print(true | false);
+  // print(true || false);
+  // print(true & false);
+  // print(true && false);
+  // print(true ^ false);
+
+  // Bitwise & Shift Operators
+  // print(~operand1);
+  // print(~operand2);
+  // print(operand1 & operand2);
+  // print(operand1 | operand2);
+  // print(operand1 ^ operand2);
+  // print(operand1 << operand2);
+  // print(operand1 >> operand2);
+}
