165b3bda86f023386985caf58837d0a16420214e
Abdurrasyid Muhasibi <com.syid@gmail.com>
2023-03-06 09:46:00 +0700

    collection

---
+++ 4. Collection/collection.dart
@@ -0,0 +1,191 @@
+void main() {
+  // var listItem = [item1, item2, item3, ...];
+  // var listObject = [1, 2, 3, 4, 5, "Hello", "World", true, false];
+  // print(listObject);
+  // var listInt = [1, 2, 3, 4, 5];
+  // print(listInt);
+  // var listString = ["Hello", "World"];
+  // print(listString);
+  // var listBool = [true, false];
+  // print(listBool);
+
+  // Empty List
+  // var listEmpty = [];
+  // var listIntEmpty = <int>[];
+  // var listStringEmpty = <String>[];
+  // var listBoolEmpty = <bool>[];
+
+  // Index of List
+  // var listObject = [1, 2, 3, 4, 5, "Hello", "World", true, false];
+  // print(listObject[0]);
+  // print(listObject[5]);
+  // print(listObject[7]);
+
+  // Check Length of List
+  // print(listObject.length);
+
+  // Add Item to List
+  // listObject.add("New Item");
+  // print(listObject);
+
+  // Add Multiple Item to List
+  // listObject.addAll(["Item 1", "Item 2", "Item 3"]);
+  // print(listObject);
+
+  // Add Item to List at Specific Index
+  // listObject.insert(0, "First Item");
+  // print(listObject);
+
+  // Remove Item from List
+  // listObject.remove("New Item");
+  // print(listObject);
+
+  // Remove Multiple Item from List
+  // listObject.removeRange(0, 5);
+  // print(listObject);
+
+  // Remove Item from List at Specific Index
+  // listObject.removeAt(0);
+  // print(listObject);
+
+  // Check Item in List
+  // print(listObject.contains("Hello"));
+
+  // Check Index of Item in List
+  // print(listObject.indexOf("Hello"));
+
+  // Remove All Item from List
+  // listObject.clear();
+  // print(listObject);
+
+  // Map List
+  // var listOfPokemon = ["Pikachu", "Bulbasaur", "Charmander", "Squirtle"];
+  // var mappedList = listOfPokemon.map((pokemon) => "I love $pokemon");
+  // print(mappedList);
+  // print(mappedList.toList());
+
+  // var set = {item1, item2, item3, ...};
+  // var setObject = {1, 2, 3, 4, 5, "Hello", "World", true, false};
+  // print(setObject);
+  // var setInt = {1, 2, 3, 4, 5};
+  // print(setInt);
+  // var setString = {"Hello", "World"};
+  // print(setString);
+  // var setBool = {true, false};
+  // print(setBool);
+
+  // Empty Set
+  // var setEmpty = <Object>{};
+  // var setIntEmpty = <int>{};
+  // var setStringEmpty = <String>{};
+  // var setBoolEmpty = <bool>{};
+
+  // Index of Set
+  // var setObject = {1, 2, 3, 4, 5, "Hello", "World", true, false};
+  // print(setObject.elementAt(0));
+  // print(setObject.elementAt(5));
+  // print(setObject.elementAt(7));
+
+  // Check Length of Set
+  // print(setObject.length);
+
+  // Add Item to Set
+  // setObject.add("New Item");
+  // print(setObject);
+
+  // Add Multiple Item to Set
+  // setObject.addAll(["Item 1", "Item 2", "Item 3"]);
+  // print(setObject);
+
+  // Remove Item from Set
+  // setObject.remove("New Item");
+  // print(setObject);
+
+  // Remove Multiple Item from Set
+  // setObject.removeWhere((item) => item == 1 || item == 2 || item == 3);
+  // print(setObject);
+
+  // Remove All Item from Set
+  // setObject.clear();
+
+  // Union of Set
+  // var set1 = {1, 2, 3, 4, 5};
+  // var set2 = {6, 7, 8, 9, 10};
+  // var set3 = set1.union(set2);
+  // print(set3);
+
+  // Intersection of Set
+  // var set4 = {1, 2, 3, 4, 5};
+  // var set5 = {3, 4, 5, 6, 7};
+  // var set6 = set4.intersection(set5);
+  // print(set6);
+
+  // Difference of Set
+  // var set7 = {1, 2, 3, 4, 5};
+  // var set8 = {3, 4, 5, 6, 7};
+  // var set9 = set7.difference(set8);
+  // print(set9);
+
+  // Map Set
+  // var setOfPokemon = {"Pikachu", "Bulbasaur", "Charmander", "Squirtle"};
+  // var mappedSet = setOfPokemon.map((pokemon) => "I love $pokemon");
+  // print(mappedSet);
+  // print(mappedSet.toSet());
+
+  // var map = {key1: value1, key2: value2, key3: value3, ...};
+  // var mapObject = {
+  //   "name": "John Doe",
+  //   "age": 20,
+  //   "isMarried": false,
+  //   "address": {"street": "Jl. ABC", "city": "Jakarta", "country": "Indonesia"}
+  // };
+  // print(mapObject);
+  // var mapInt = {1: "One", 2: "Two", 3: "Three"};
+  // print(mapInt);
+  // var mapString = {"name": "John Doe", "age": 20};
+  // print(mapString);
+  // var mapBool = {true: "True", false: "False"};
+  // print(mapBool);
+
+  // Empty Map
+  // var mapEmpty1 = <String, dynamic>{};
+  // var mapEmpty2 = Map<String, dynamic>();
+  // Map<String, dynamic> mapEmpty3 = {};
+
+  // Check Length of Map
+  // print(mapObject.length);
+
+  // Check Key in Map
+  // print(mapObject.containsKey("name"));
+
+  // Check Value in Map
+  // print(mapObject.containsValue("John Doe"));
+
+  // Get Value from Map
+  // print(mapObject["name"]);
+
+  // Get All Key from Map
+  // print(mapObject.keys);
+
+  // Get All Value from Map
+  // print(mapObject.values);
+
+  // Add Item to Map
+  // mapObject["hobby"] = "Playing Game";
+  // print(mapObject);
+
+  // Add Multiple Item to Map
+  // mapObject.addAll({"phone": "08123456789", "email": "johndoe@gmail.com"});
+  // print(mapObject);
+
+  // Remove Item from Map
+  // mapObject.remove("hobby");
+  // print(mapObject);
+
+  // Remove Multiple Item from Map
+  // mapObject.removeWhere((key, value) => key == "phone" || key == "email");
+  // print(mapObject);
+
+  // Remove All Item from Map
+  // mapObject.clear();
+}
